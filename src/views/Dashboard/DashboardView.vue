574d73b47fc4d74079e4d567c70ab04bb674d425
Chuck <ml28888@126.com>
2022-05-12 15:13:14 +0800

    remove TheHead

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import TheHead from '@/components/TheHead/TheHead.vue'
 import StatisticsBar from './components/StatisticsBar.vue'
 import TotalBar from './components/TotalBar.vue'
 import LineChart from './components/LineChart.vue'
